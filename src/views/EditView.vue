a249767f5e852890898fe03bd54fce255ae1571a
carrynong2 <carrynong2@gmail.com>
2023-12-24 16:13:32 +0700

    implement edit, delete

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,3 +1,62 @@
+<script setup>
+  import { useTodoStore } from "../stores/todo"
+  import { onMounted, ref } from "vue";
+  import { useRoute, RouterLink } from "vue-router";
+
+  const todoStore = useTodoStore()
+  const route = useRoute()
+  const isLoaded = ref(false)
+  const todoId = ref(-1)
+
+  onMounted(async () => {
+    await todoStore.loadTodo(route.params.id)
+    isLoaded.value = true
+    todoId.value = parseInt(route.params.id)
+  })
+
+  const editTodo = async (selectedTodo) => {
+    try {
+      const bodyData = {
+        name: selectedTodo.name,
+        status: selectedTodo.status
+      }
+      await todoStore.editTodo(bodyData, todoId.value);
+      alert("edit complete!!")
+    } catch(error) {
+      console.log("error", error);
+    }
+  }
+
+</script>
+
+
 <template>
-  <div>Edit View</div>
+  <div>
+    Edit 
+    <RouterLink :to="{name: 'todo-list'}">
+      Back
+    </RouterLink>
+    <div v-if="isLoaded">
+      id {{ todoId }}
+      <div>
+        Name
+        <input type="text" v-model="todoStore.selectedTodo.name">
+      </div>
+      <div>
+        Status
+        <select  v-model="todoStore.selectedTodo.status" >
+          <option value="">Select status</option>
+          <option v-for="status in todoStore.statuses" :value="status">
+            {{ status }}
+          </option>
+        </select>
+        <div>
+          <button @click="editTodo(todoStore.selectedTodo)">Edit</button>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      isLoading
+    </div>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,22 @@
 <script setup>
   import { onMounted, ref } from "vue";
   import { useTodoStore } from "../stores/todo"
+  import {RouterLink} from "vue-router"
   const todoStore = useTodoStore()
   const todoText = ref("")
+  const isLoading = ref(false)
 
   onMounted(async () => {
+    isLoading.value = true
     await todoStore.loadTodos();
+    isLoading.value = false
   })
 
   const addTodo = async (todoText) => {
     try {
+      isLoading.value = true
       await todoStore.addTodo(todoText);
+      isLoading.value = false
     } catch(error) {
       console.log("error", error);
     }
@@ -18,11 +24,24 @@
 
   const editStatus = async(todoData, todoId) => {
     try {
+      isLoading.value = true
       const updateData = {
         name: todoData.name,
         status: todoData.status
       }
       await todoStore.editTodo(updateData, todoId);
+      isLoading.value = false
+    } catch(error) {
+      console.log("error", error);
+    }
+  }
+
+  const deleteTodo = async(todoId) => {
+    try {
+      isLoading.value = true
+      await todoStore.removeTodo(todoId);
+      await todoStore.loadTodos();
+      isLoading.value = false
     } catch(error) {
       console.log("error", error);
     }
@@ -36,6 +55,9 @@
       <input type="text" v-model="todoText">
       <button @click="addTodo(todoText)">Add</button>
     </div>
+    <div v-if="isLoading">
+      <h2>Loading</h2>
+    </div>
     <ul>
       <li v-for="todo in todoStore.list">
         {{ todo.name }} 
@@ -47,8 +69,10 @@
           </option>
         </select>
         
-        <button>Edit</button>
-        <button>Delete</button>
+        <RouterLink :to="{name: 'edit-todo', params: {id: todo.id}}">
+          <button>Edit</button>
+        </RouterLink>
+        <button @click="deleteTodo(todo.id)">Delete</button>
       </li>
     </ul>
   </div>
